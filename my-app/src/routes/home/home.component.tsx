409f2a9bf223cc29068cd80b3373e93d6a1973d7
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-05-12 14:50:50 +0200

    issues solved @types aos installed

--- my-app/src/routes/home/home.component.tsx
+++ my-app/src/routes/home/home.component.tsx
@@ -1,25 +1,71 @@
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import './home.style.scss'
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import CountUp from 'react-countup';
 
 const Home = () => {
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
-      <h1>Landing Page</h1>
-      <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
-      <h1>Landing Page</h1>
-      <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
-      <h1>Landing Page</h1>
-      <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
-      <h1>Landing Page</h1>
-      <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
-      <h1>Landing Page</h1>
-      <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
-      <h1>Landing Page</h1>
-      <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
-      <h1>Landing Page</h1>
-      <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
-    <Outlet />
+    <div className='home-wrapper'>
+      <CountUp 
+      start={0}
+      end={2000}
+      duration={2.75}
+      />
+      <div data-aos="fade-right">
+        <h1>Landing Page</h1>
+      </div>
+      <div data-aos="fade-left">
+        <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
+      </div>
+
+      <div data-aos="fade-right">
+        <h1>Landing Page</h1>
+      </div>
+      <div data-aos="fade-left">
+        <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
+      </div>
+
+      <div data-aos="fade-right">
+        <h1>Landing Page</h1>
+      </div>
+      <div data-aos="fade-left">
+        <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
+      </div>
+
+      <div data-aos="fade-right">
+        <h1>Landing Page</h1>
+      </div>
+      <div data-aos="fade-left">
+        <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
+      </div>
+
+      <div data-aos="fade-right">
+        <h1>Landing Page</h1>
+        <CountUp 
+      start={0}
+      end={2000}
+      duration={2.75}
+      redraw={true}
+      />
+
+      
+
+      </div>
+      <div data-aos="fade-left">
+        <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
+      </div>
+   
+     
+
+      <Outlet />
     </div>
   )
 }
