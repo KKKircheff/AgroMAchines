a1d39e160c6a78041e892dd94a2dceb8e9c1ada1
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-05-13 10:02:34 +0200

    Render-if-visible integrated

--- my-app/src/routes/home/home.component.tsx
+++ my-app/src/routes/home/home.component.tsx
@@ -4,6 +4,7 @@ import './home.style.scss'
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import CountUp from 'react-countup';
+import RenderIfVisible from 'react-render-if-visible';
 
 const Home = () => {
 
@@ -14,10 +15,10 @@ const Home = () => {
 
   return (
     <div className='home-wrapper'>
-      <CountUp 
-      start={0}
-      end={2000}
-      duration={2.75}
+      <CountUp
+        start={0}
+        end={2000}
+        duration={2.75}
       />
       <div data-aos="fade-right">
         <h1>Landing Page</h1>
@@ -47,23 +48,27 @@ const Home = () => {
         <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
       </div>
 
+      <RenderIfVisible visibleOffset={20}>
+        <div data-aos="fade-right">
+          <h1>Landing Page</h1>
+          <CountUp
+            start={0}
+            end={2000}
+            duration={2.75}
+            redraw={true}
+          />
+        </div>
+        <div data-aos="fade-left">
+          <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
+        </div>
+      </RenderIfVisible >
       <div data-aos="fade-right">
         <h1>Landing Page</h1>
-        <CountUp 
-      start={0}
-      end={2000}
-      duration={2.75}
-      redraw={true}
-      />
-
-      
-
       </div>
       <div data-aos="fade-left">
         <img src="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Coffee" />
       </div>
-   
-     
+
 
       <Outlet />
     </div>
