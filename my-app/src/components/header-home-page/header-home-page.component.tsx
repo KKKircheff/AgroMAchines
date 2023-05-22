82e53e38c3616ccf0965f00b61c756a3bff278c7
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-05-22 20:47:38 +0200

    push before branching

---
+++ my-app/src/components/header-home-page/header-home-page.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header-home-page.style.scss'
+
+const HeaderHomePage = () => {
+  return (
+    <div className="header-home-page-wrapper">
+        <h1>Поливни системи втора употреба от Нидерландия</h1>
+        <p>Some Image</p>
+    </div>
+  )
+}
+
+export default HeaderHomePage
