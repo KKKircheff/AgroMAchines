87bbe03f9279be13fab6f33d58eb794e0c3610dc
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-05-26 22:47:27 +0200

    Form validation functionalities

--- my-app/src/components/contact-form/contact-form.component.tsx
+++ my-app/src/components/contact-form/contact-form.component.tsx
@@ -21,54 +21,68 @@ interface FieldsRegex {
 const fieldsRegex: FieldsRegex = {
   name: /^[A-Za-z\s]{0,35}$/,
   company: /^[A-Za-z\s\d-]{0,35}$/,
-  email: /^[A-Za-z0-9._%+-@]{0,15}$/,
-  phoneNumber:/^[0-9+\-()\s]{0,15}$/,
+  email: /^[A-Za-z0-9._%+-@]{0,40}$/,
+  phoneNumber:/^[0-9+\-()\s]{0,20}$/,
   message: /^.{0,400}$/,
-  fullPhoneNumber: /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/g,
-  fullEmailName: /^(?!.{41})[a-z0-9]+(?:\.[a-z0-9]+)*@[a-z0-9-]+(?:\.[a-z0-9-]+)*\.[a-zA-Z]{2,6}$/gm
+  fullPhoneNumber: /^\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}$/,
+  fullEmailAddress: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
 
 export default function ContatForm() {
 
   const navigate = useNavigate();
   const [formValues, setFormValues] = useState(contactInfo);
+  const [isFormValid, setIsFormValid] = useState(true);
+  const [errorMessage,setErrorMessage] = useState('');
 
   const clearFormFields = () => {
-    console.log('clearfields')
+    setFormValues(contactInfo);
   }
 
-
   const validateField = (regex: RegExp, value: string) => {
-    return regex.test(value);
+     return regex.test(value);
   }
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     const { name, value } = event.target;
+    setIsFormValid(true);
     const isValidFieldValue = validateField(fieldsRegex[name], value);
-    console.log(isValidFieldValue)
-    if (!isValidFieldValue) {
-       return 
-      }
 
+    if (!isValidFieldValue) {
+               return 
+           }
     setFormValues({
-      ...formValues,
-      [name]: value,
-    });
+         ...formValues,
+         [name]: value,
+         });
   }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
     const { name, email, message, phoneNumber } = formValues;
+
     if (!name || !email || !phoneNumber || !message) {
-      alert("Моля попълнете всички задължителни полете!");
+      setIsFormValid(false);
+      setErrorMessage("Моля попълнете всички задължителни полетa във формата!");
       return;
     }
+    if (!validateField(fieldsRegex.fullEmailAddress,email)) {
+      setIsFormValid(false);
+      setErrorMessage('Грешно въведен мейл адреса!');
+      return
+    }
+    if (!validateField(fieldsRegex.fullPhoneNumber,phoneNumber)) {
+      setIsFormValid(false);
+      setErrorMessage('Грешно въведен телефонен номер!');
+      return
+    }
+    clearFormFields();
 
-    console.log('Form:-----------', formValues);
   }
 
   return (
-    <div className="contact-form-container">
+    <div className="contact-form-container" data-aos="fade-right">
       <h2>Контакт с нас</h2>
       <p>ако имате въпрос относно някой от нашите продукти или имате нужда от съдействие в Нидерландия за закупуване на земеделска техника</p>
       <form onSubmit={handleSubmit}>
@@ -81,7 +95,7 @@ export default function ContatForm() {
         />
 
         <FormInputField
-          label='Компния'
+          label='Компния /опция/'
           type='text'
           name='company'
           onChange={handleChange}
@@ -117,6 +131,7 @@ export default function ContatForm() {
         <div className="buttons-container">
           <Button type='submit' buttonType={'default'}>изпрати</Button>
         </div>
+       { !isFormValid && <div className="form-error-message">{errorMessage}</div>}
       </form>
     </div>
   )
