50c7e62ffa8d1566d8666fe6a4b600bb335ae221
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-05-27 13:05:57 +0200

    Carousell styled. Utils folder custom styles

--- my-app/src/components/product-card/product-card.component.tsx
+++ my-app/src/components/product-card/product-card.component.tsx
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, {CSSProperties} from 'react';
 import { isMobile } from 'react-device-detect';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-
-
+import { renderArrowNext, renderArrowPrev, renderIndicator } from '../../utils/custom_carousel_controls';
 import './product.card.style.scss';
 
 type CardProps = {
@@ -16,13 +15,79 @@ type CardProps = {
 
 const ProductCard = ({ url, title, subtitle, content, imgSide }: CardProps) => {
 
+//   const arrowStyles: CSSProperties = {
+//     position: 'absolute',
+//     zIndex: 2,
+//     top: 'calc(50% - 15px)',
+//     width: 30,
+//     height: 30,
+//     cursor: 'pointer',
+// };
+
+// const indicatorStyles: CSSProperties = {
+//     background: '#fff',
+//     width: 8,
+//     height: 8,
+//     display: 'inline-block',
+//     margin: '0 8px',
+// };
+
+// const renderArrowPrev = (onClickHandler: () => void, hasPrev: boolean, label: string): React.ReactNode => {
+//   return hasPrev ? (
+//     <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
+//       <SlArrowLeft />
+//     </button>
+//   ) : null;
+// };
+// const renderArrowNext = (onClickHandler: () => void, hasNext: boolean, label: string): React.ReactNode => {
+//   return hasNext ? (
+//     <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
+//       <SlArrowRight />
+//     </button>
+//   ) : null;
+// };
+
+// const renderIndicator = (
+//   onClickHandler: (e: React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<Element>) => void,
+//   isSelected: boolean,
+//   index: number,
+//   label: string
+// ): React.ReactNode => {
+//   if (isSelected) {
+//     return (
+//       <li
+//         style={{ ...indicatorStyles, background: '#000' }}
+//         aria-label={`Selected: ${label} ${index + 1}`}
+//         title={`Selected: ${label} ${index + 1}`}
+//       />
+//     );
+//   }
+//   return (
+//     <li
+//       style={indicatorStyles}
+//       onClick={onClickHandler}
+//       onKeyDown={onClickHandler}
+//       value={index}
+//       key={index}
+//       role="button"
+//       tabIndex={0}
+//       title={`${label} ${index + 1}`}
+//       aria-label={`${label} ${index + 1}`}
+//     />
+//   );
+// };
+
+   console.log(isMobile);
   return (
     <div className={`product-card ${imgSide}`} data-aos="fade-in">
       <div className="carousel-wrapper">
         <Carousel
-          emulateTouch={isMobile}
+          renderArrowPrev={renderArrowPrev}
+          renderArrowNext={renderArrowNext}
+          renderIndicator={renderIndicator}
+          emulateTouch={true}
           showThumbs={false}
-          swipeable={isMobile}
+          swipeable={true}
           showStatus={false}
           showArrows={!isMobile}
         >
