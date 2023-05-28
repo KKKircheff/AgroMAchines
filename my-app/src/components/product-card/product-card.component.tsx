854624946143ef315748ae154520f9f0029b8875
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-05-28 16:02:58 +0200

    product-object integrated

--- my-app/src/components/product-card/product-card.component.tsx
+++ my-app/src/components/product-card/product-card.component.tsx
@@ -8,16 +8,17 @@ import { useEffect } from 'react';
 import {GiWaterRecycling} from 'react-icons/gi'
 import AOS from 'aos';
 
-
 type CardProps = {
-  url: string,
+  url: string[],
+  mainTitle: string;
   title: string,
   subtitle: string,
   content: string,
+  price:number,
   imgSide: 'left' | 'right',
 }
 
-const ProductCard = ({ url, title, subtitle, content, imgSide }: CardProps) => {
+const ProductCard = ({ url, mainTitle, title, subtitle, content, price, imgSide }: CardProps) => {
 
   useEffect(() => {
     AOS.init({ duration: 900 });
@@ -38,29 +39,27 @@ const ProductCard = ({ url, title, subtitle, content, imgSide }: CardProps) => {
           showStatus={false}
           showArrows={!isMobile}
         >
-          <div className='test-div'>
-            <img src={url} alt='поливна макра / система втора употреба снимки' />
-            {/* <p className="legend">Legend 1</p> */}
-          </div>
-          <div>
-            <img src={url} alt='поливна макра / система втора употреба снимки' />
-            {/* <p className="legend">Legend 2</p> */}
-          </div>
-          <div>
-            <img src={url} alt='поливна макра / система  втора употреба снимки' />
-            {/* <p className="legend">Legend 3</p> */}
-          </div>
+          {
+          url.map((imageUrl,index)=> {
+               return (<div 
+                       key={index}
+                       className='test-div'>
+                          <img src={imageUrl} alt='поливна макра / система втора употреба снимки' />
+                         {/* <p className="legend">Legend 1</p> */}
+                      </div>
+                  )})
+            }
         </Carousel>
       </div>
       <div className="content-section" data-aos="fade-down" >
         <div className="top-banner-title-section" data-aos="fade-right"><span>избрана оферта</span></div>
         <div className="top-banner-icon-section" data-aos="fade-left"><span><GiWaterRecycling /></span></div>
         <div className="title-group">
-          <h2 className='title'>Макара за напояване</h2>
+          <h2 className='title'>{mainTitle}</h2>
           <h2 className='subtitle'>Mарка: <span>{title}</span></h2>
           <h3 className="subtitle">Помпа: <span>{subtitle}</span></h3>
         </div>
-        <h4>Описане:</h4>
+        <h4>Цена: <span>{price} лева</span> </h4>
          <p className="content">{content}</p>
       </div>
     </div>
--- my-app/src/routes/home/home.component.tsx
+++ my-app/src/routes/home/home.component.tsx
@@ -5,17 +5,15 @@ import './home.style.scss'
 
 import AOS from 'aos';
 import "aos/dist/aos.css";
-import CountUp from 'react-countup';
 import RenderIfVisible from 'react-render-if-visible';
 
 import Button from '../../components/button/button.component';
 import HeaderHomePage from '../../components/header-home-page/header-home-page.component';
 import InfoSection from '../../components/info-section/info-section.component';
-import ContentCard from '../../components/content-card/content-card.component';
 import ContatForm from '../../components/contact-form/contact-form.component';
 import ProductCard from '../../components/product-card/product-card.component';
 import FooterHomePage from '../../components/footer-home-page/footer-home-page.component';
-
+import { productsData } from '../../application-data/products-data';
 
 
 const Home = () => {
@@ -31,13 +29,19 @@ const Home = () => {
       <HeaderHomePage />
       <InfoSection />
       <ContatForm />
-      <ProductCard
-        url='https://i.ibb.co/481qkwP/img-reel-2.jpg'
-        title='Rainbow'
-        subtitle='Rowatti'
-        content='Поливна макара Irrimec ф90, 360 м. - редуктор с турбина. Лафетен лагер. Помпа Rowatti 80 m3, 9 bar. Плюс комплект маркучи. Много запазена и поддържана. Готова за ползване. Внос Нидерландия. Пистолет Twin spruier '
-        imgSide='left'
+
+      { productsData 
+       && productsData.map((product, index)=><ProductCard
+          key={index}
+          url={product.url}
+          mainTitle={product.mainTitle}
+          title={product.title}
+          subtitle={product.subtitle}
+          content={product.content}
+          price={product.price}
+          imgSide='left'
       />
+      )}
       <FooterHomePage />
       <Outlet />
     </div>
