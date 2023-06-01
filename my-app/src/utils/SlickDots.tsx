78b1d52c4e6f39b01095f82198afc47a0c836754
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-06-01 12:34:58 +0200

    dots ready

--- my-app/src/utils/SlickDots.tsx
+++ my-app/src/utils/SlickDots.tsx
@@ -1,23 +1,11 @@
 import './SlickDots.scss';
 
-export default function SlickDots (dots:any){
-    return  (
-            <div 
-              className="slick-dots-wrapper"
-              style={{
-                margin:'0 0 3rem 0'
-              }}
-            >
-              <ul 
-              style={{ 
-                color: 'white',
-                //  background: '#76B82A',
-                //  border:'1px solid white',
-                //  width: '80%',
-                //  height: '20vw',
-                //  display: 'inline-block',
-                //  margin: '0 8px',
-                 }}>{dots}</ul>
-            </div>
-          )
+export default function SlickDots(dots: any) {
+    return (
+        <div style={{ margin: '0 0 2.5rem 0' }}>
+            <ul className="slick-dots-wrapper">
+                {dots}
+            </ul>
+        </div>
+    )
 }
